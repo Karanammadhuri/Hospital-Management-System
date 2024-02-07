f33fd7f52fb454246318c0fef02235648a52772f
Madhuri <136251763+Karanammadhuri@users.noreply.github.com>
2024-02-07 08:56:31 -0500

    0013_remove_patient_pgender.py

--- hospitalmanagementapp/migrations/0013_remove_patient_pgender.py
+++ hospitalmanagementapp/migrations/0013_remove_patient_pgender.py
@@ -6,7 +6,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('hospiapp', '0012_patient_pgender'),
+        ('hospitalmanagementapp', '0012_patient_pgender'),
     ]
 
     operations = [
