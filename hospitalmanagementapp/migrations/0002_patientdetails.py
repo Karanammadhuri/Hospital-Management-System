9b8dc9f54cfe93803d789660288438b00e0dfccd
Madhuri <136251763+Karanammadhuri@users.noreply.github.com>
2024-02-07 08:52:26 -0500

    0002_patientdetails.py

--- hospitalmanagementapp/migrations/0002_patientdetails.py
+++ hospitalmanagementapp/migrations/0002_patientdetails.py
@@ -5,7 +5,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('hospiapp', '0001_initial'),
+        ('hospitalmanagementapp', '0001_initial'),
     ]
 
     operations = [
